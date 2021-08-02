5c3546abd51ff1cba3c81d1cffc071ccc48406e0
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2021-08-02 17:35:03 +0800

    added: routes and controller for User registration, and test in postman

---
+++ backend/app/Models/Student.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Student extends Model
+{
+    use HasFactory, Notifiable, HasApiTokens;
+
+    // mass assignment
+    protected $fillable = [
+      'name',
+      'username',
+      'email',
+      'thumbnail',
+      'password'
+    ];
+
+    protected $hidden = [
+      'password',
+      'remember_token'
+    ];
+
+    protected $casts = [
+      'email_verified_at' => 'datetime'
+    ];
+}
