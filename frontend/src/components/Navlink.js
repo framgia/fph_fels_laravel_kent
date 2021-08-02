fede72516970903dbb50f0f49ff5af962310c2a2
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2021-08-02 13:00:39 +0800

    added: register page

--- frontend/src/components/Navlink.js
+++ frontend/src/components/Navlink.js
@@ -2,7 +2,11 @@ export default function Navlink(props) {
   return (
     <>
       <li className="nav-item text-center">
+<<<<<<< HEAD
         <a className="nav-link navLink" href={props.navLink}>{props.icon}{props.navName}</a>
+=======
+        <a className={props.navClass} href={props.navLink}>{props.icon}{props.navName}</a>
+>>>>>>> 35ddf48 (added: register page)
       </li>
     </>
   );
